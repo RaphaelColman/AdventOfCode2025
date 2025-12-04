05e458c90e7b9d48620f89e6beccb36cb39625fe
Raphael <rafi.colman@googlemail.com>
2025-12-04 14:52:43 +0000

    Day 4 part 1

--- src/Solutions/Day4.hs
+++ src/Solutions/Day4.hs
@@ -1,21 +1,46 @@
 module Solutions.Day4
-  ( aoc4
-  ) where
+  ( aoc4,
+  )
+where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+    printTestSolutions,
+  )
+import Common.Geometry (Grid, Point, enumerateMultilineStringToVectorMap, gridNeighbours)
+import Control.Applicative.Combinators (some)
+import Control.Monad.Reader (MonadReader (ask), Reader, filterM, runReader)
+import qualified Data.Map.Strict as M
+import Debug.Trace
+import Text.Trifecta (CharParsing (anyChar), Parser)
 
 aoc4 :: IO ()
 aoc4 = do
   printSolutions 4 $ MkAoCSolution parseInput part1
-  printSolutions 4 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+-- printSolutions 4 $ MkAoCSolution parseInput part2
 
-part1 :: String -> String
-part1 = undefined
+parseInput :: Parser (Grid Char)
+parseInput = do
+  allChars <- some anyChar
+  pure $ enumerateMultilineStringToVectorMap allChars
+
+part1 = runReader solve
 
 part2 :: String -> String
 part2 = undefined
+
+accessible :: Point -> Reader (Grid Char) Bool
+accessible point = do
+  grid <- ask
+  let neighbours = gridNeighbours grid point
+  let neighbourRolls = filter (== '@') $ M.elems neighbours
+  pure $ length neighbourRolls < 4 && grid M.! point == '@'
+
+solve :: Reader (Grid Char) Integer
+solve = do
+  grid <- ask
+  let points = M.keys grid
+  accessiblePoints <- filterM accessible points
+  pure $ toInteger $ length accessiblePoints
