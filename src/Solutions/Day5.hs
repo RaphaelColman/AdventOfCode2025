148c45225e454f84b83abc8b13d93034a0cfc6ed
Raphael <rafi.colman@googlemail.com>
2025-12-05 09:42:24 +0000

    Day 5 part 1

--- src/Solutions/Day5.hs
+++ src/Solutions/Day5.hs
@@ -1,21 +1,43 @@
 module Solutions.Day5
-  ( aoc5
-  ) where
+  ( aoc5,
+  )
+where
 
-import           Common.AoCSolutions (AoCSolution (MkAoCSolution),
-                                      printSolutions)
-import           Text.Trifecta       (Parser)
+import Common.AoCSolutions
+  ( AoCSolution (MkAoCSolution),
+    printSolutions,
+    printTestSolutions,
+  )
+import Data.Range
+import Text.Parser.Char
+import Text.Trifecta (Parser, integer, some, token, manyTill, integer')
+import Control.Monad (void)
+import Text.Parser.LookAhead (LookAheadParsing(lookAhead))
 
 aoc5 :: IO ()
 aoc5 = do
-  printSolutions 5 $ MkAoCSolution parseInput part1
+  --printSolutions 5 $ MkAoCSolution parseInput part1
   printSolutions 5 $ MkAoCSolution parseInput part2
 
-parseInput :: Parser String
-parseInput = undefined
+parseInput :: Parser ([Range Integer], [Integer])
+parseInput = do
+  ranges <- parseRanges
+  ingredients <- some integer
+  pure (ranges, ingredients)
 
-part1 :: String -> String
-part1 = undefined
+parseRanges :: Parser [Range Integer]
+parseRanges = do
+  manyTill parseIngredientRange (newline)
 
-part2 :: String -> String
-part2 = undefined
+parseIngredientRange :: Parser (Range Integer)
+parseIngredientRange = do
+  start <- integer'
+  char '-'
+  end <- integer'
+  newline
+  pure $ start +=+ end
+
+part1 (ranges, ingredientIds) = length $ filter (inRanges ranges) ingredientIds
+
+part2 (ranges, _) = length $ mergeRanges ranges
+--I need to get the size of each range and sum them up.
