5a5ee679f3ff89a98cb07aafc3209bea428a81fa
Raphael <rafi.colman@googlemail.com>
2025-12-10 15:27:09 +0000

    Day 8 part 1

--- src/Solutions/Day8.hs
+++ src/Solutions/Day8.hs
@@ -14,9 +14,9 @@ import Common.AoCSolutions
   )
 import Common.FunctorUtils (fmap2, fmap3)
 import Control.Lens
-import Data.Foldable (minimumBy)
+import Data.Foldable (Foldable (foldl'), minimumBy)
 import Data.Function (on)
-import Data.List (sortBy)
+import Data.List (sort, sortBy)
 import Data.Map.Strict (Map)
 import qualified Data.Map.Strict as M
 import Data.Ord (Down (..), comparing)
@@ -45,7 +45,7 @@ makeLenses ''CircuitMapState
 
 aoc8 :: IO ()
 aoc8 = do
-  printTestSolutions 8 $ MkAoCSolution parseInput part1
+  printSolutions 8 $ MkAoCSolution parseInput part1
 
 -- printSolutions 8 $ MkAoCSolution parseInput part2
 
@@ -55,20 +55,24 @@ parseInput = do
     [x, y, z] <- commaSep integer
     pure $ V3 x y z
 
-part1 = buildCircuitMap
+part1 nodes =
+  buildCircuitMap nodes
+    & addUnconnectedNodes nodes
+    & _circuitMap
+    & checkSum
 
 -- | Order all pairs of points by their distance from each other, from largest to smallest
 orderPairsByDistance :: [V3 Integer] -> [(V3 Integer, V3 Integer)]
-orderPairsByDistance points = sortBy (comparing (Down . uncurry distanceF)) allpairs
+orderPairsByDistance points = sortBy (comparing (uncurry distanceF)) allpairs
   where
     allpairs = map (\[x, y] -> (x, y)) $ tuples 2 points
     distanceF :: V3 Integer -> V3 Integer -> Double
     distanceF p1' p2' = distance (fromIntegral <$> p1') (fromIntegral <$> p2')
 
 buildCircuitMap :: [V3 Integer] -> CircuitMapState
-buildCircuitMap nodes = foldr processPair initialState orderedPairs
+buildCircuitMap nodes = foldl' (flip processPair) initialState someOrderedPairs
   where
-    orderedPairs = orderPairsByDistance nodes
+    (someOrderedPairs, remainingOrderedPairs) = splitAt 1000 $ orderPairsByDistance nodes
     initialState = MkCircuitMapState 0 M.empty
 
 processPair :: (V3 Integer, V3 Integer) -> CircuitMapState -> CircuitMapState
@@ -76,30 +80,51 @@ processPair (p1, p2) state = case (c1, c2) of
   (Nothing, Nothing) ->
     -- Neither point is in a circuit yet, create a new circuit
     -- trace (printf "None connect. P1: %s, P2: %s, state: %s" (show p1) (show p2) (show state)) $
-      state
-        & nextCircuitId %~ (+ 1)
-        & circuitMap
-          %~ M.insert p1 (state ^. nextCircuitId)
-            . M.insert p2 (state ^. nextCircuitId)
+    state
+      & nextCircuitId %~ (+ 1)
+      & circuitMap
+        %~ M.insert p1 (state ^. nextCircuitId)
+          . M.insert p2 (state ^. nextCircuitId)
   (Just id1, Just id2) ->
     -- Both points are already in circuits. These two circuits must be merged
     -- ah wait. We need to check the actual ids. If they are the same, we do nothing? Wait isn't that's what is happening anyway?
     -- trace (printf "Both connect. P1: %s, P2: %s, state: %s" (show p1) (show p2) (show state)) $
-      state
-        & circuitMap %~ M.map (\cid -> if cid == id2 then id1 else cid)
-        & circuitMap %~ M.insert p1 id1 . M.insert p2 id1 -- Insert both of these points using id1 (the merge survivor)
+    state
+      & circuitMap %~ M.map (\cid -> if cid == id2 then id1 else cid)
+      & circuitMap %~ M.insert p1 id1 . M.insert p2 id1 -- Insert both of these points using id1 (the merge survivor)
   (Just cid, Nothing) ->
     -- p1 is already in a circuit, add p2 to that circuit
     -- trace (printf "1 connects. P1: %s, P2: %s, state: %s" (show p1) (show p2) (show state)) $
-      state
-        & circuitMap %~ M.insert p2 cid
+    state
+      & circuitMap %~ M.insert p2 cid
   (Nothing, Just cid) ->
     -- p2 is already in a circuit, add p1 to that circuit
     -- trace (printf "2 connects. P1: %s, P2: %s, state: %s" (show p1) (show p2) (show state)) $
-      state
-        & circuitMap %~ M.insert p1 cid
+    state
+      & circuitMap %~ M.insert p1 cid
   where
     c1 = M.lookup p1 $ state ^. circuitMap
     c2 = M.lookup p2 $ state ^. circuitMap
-    dbgline :: CircuitMapState -> String
-    dbgline state = printf "P1: %s, P2: %s, state: %s" (show p1) (show p2) (show state)
+
+addUnconnectedNodes :: [V3 Integer] -> CircuitMapState -> CircuitMapState
+addUnconnectedNodes nodes state = foldr f state nodes
+  where
+    f :: V3 Integer -> CircuitMapState -> CircuitMapState
+    f node st =
+      if node `M.notMember` (st ^. circuitMap)
+        then
+          st
+            & nextCircuitId %~ (+ 1)
+            & circuitMap %~ M.insert node (st ^. nextCircuitId)
+        else st
+
+countCircuits :: CircuitMap -> Map Integer Integer
+countCircuits = M.foldrWithKey' f M.empty
+  where
+    f :: V3 Integer -> Integer -> Map Integer Integer -> Map Integer Integer
+    f node cid = M.insertWith (+) cid 1
+
+checkSum mp = snd <$> sorted & take 3 & product
+  where
+    counts = M.toList $ countCircuits mp
+    sorted = sortBy (comparing (Down . snd)) counts
